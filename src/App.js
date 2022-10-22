1343e5a2aea5cce39d1bd570915d3fc2b93c4f2c
Caleb Sanchez <caleb.sanchez.s@hotmail.com>
2022-10-21 22:26:46 -0600

    Dependencies and App.js updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit and save to reload.
         </p>
         <a
           className="App-link"
